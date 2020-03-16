d92a7268cae0b26aa58b46bd330521e53b4e8de9
Kaiylar <haiderweirdman@gmail.com>
2020-03-16 21:16:41 +0000

    Initial commit

---
+++ ChatClient.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+
+public class ChatClient {
+  private Socket server;
+
+  //Constructor method for client
+  public ChatClient(String address, int port) {
+    try {
+      server = new Socket(address, port);
+    }
+    catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    new EchoClient("localhost", 14002).go();
+  }
+
+
+}
---
+++ ChatServer.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+
+public class ChatServer {
+  private ServerSocket input;
+}
+
+public static void main(String[] args) {
+  new EchoServer(14002).go();
+}
